cc0019b8a317999ba6ab46c6b654fffa2904ec15
python533 <143389231+python533@users.noreply.github.com>
2024-02-11 19:19:22 +0300

    PHP site
    Geliştirmeye Açık bir site Template'i.

---
+++ index.php
@@ -0,0 +1,69 @@
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+<meta charset="utf-8">
+<title></title>
+<link rel="stylesheet" href="css/main.CSS">
+</head>
+<body>
+<div class="layout-wrapper">
+<header class="layout-header" data-title="header">
+<a href="https://www.google.com/" class="logo">LOGO</a>
+<ul class="layout-header-menu">
+<li><a href="#"></a>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </li>
+<li><a href="#"></a>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </li>
+<li><a href="#"></a>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </li>
+<li><a href="#"></a>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </li>
+</ul>
+</header>
+
+<div class="sidebar" data-title="#sidebar">
+<h2 class="sidebar-title" data-title="#sidebar">SIDEBAR TITLE</h2>
+<ul>
+<li><a href="#"></a>Sidebar Lİnk1 </li>
+<li><a href="#"></a>Sidebar Lİnk2 </li>
+<li><a href="#"></a>Sidebar Lİnk3 </li>
+<li><a href="#"></a>Sidebar Lİnk4 </li>
+</ul>
+</div>
+
+
+
+<div class="main-content" data-title="#main-content">
+<h2>Lorem Ipsum</h2>
+<ul>
+<li><a href="#"></a>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </li>
+<li><a href="#"></a>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </li>
+<li><a href="#"></a>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </li>
+<li><a href="#"></a>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </li>
+</ul>
+</div>
+
+
+
+
+<footer class="layout-footer" data-title="#footer">
+        <ul class="layout-footer-menu">
+            <li><a href="#">FOOTER LINK</a></li>
+            <li><a href="#">FOOTER LINK</a></li>
+            <li><a href="#">FOOTER LINK</a></li>
+            <li><a href="#">FOOTER LINK</a></li>
+            <li><a href="#">FOOTER LINK</a></li>
+            <li><a href="#">FOOTER LINK</a></li>
+            <li><a href="#">FOOTER LINK</a></li>
+        </ul>
+        <p class="layout-copyright">Telif Hakkı © 2021 webmetreçcom. Tüm hakları saklıdır.</p>
+    </footer>
+
+
+
+
+
+
+
+
+</div>
+</body>
+</html>
